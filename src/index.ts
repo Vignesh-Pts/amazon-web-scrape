2a9f1455047764644eb83f891703845e735eaa06
Vignesh Thirunavukarasu <bharathi.e@sacumen.com>
2024-08-26 11:07:57 +0530

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Browser } from 'puppeteer';
 import { PurchasedProducts } from './interface/purchased-products';
 
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 const signInUrl: string = 'https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0';
 const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
